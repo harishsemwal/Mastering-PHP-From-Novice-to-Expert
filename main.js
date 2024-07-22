92241dce690b2362bc85d53d2445cd3dfa55f872
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-22 23:17:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 // Where we can embedded js instead of php
 console.log("JavaScript");
+function hackWebsite() {
+var vulnerability = true;
+
+    if (vulnerability) {
+        console.log("Exploiting vulnerability...");
+        console.log("Access granted!");
+        // Now we can do something "malicious"
+        document.body.innerHTML = "<h1>Hacked!</h1><p>Your website is now under my control.</p>";
+    } else {
+        console.log("No vulnerability found.");
+    }
+}
+
+hackWebsite();
